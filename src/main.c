22f8f21ca65802d7718217751735588255ffbbc2
Raed <raed.riseup.net>
2015-04-30 19:31:15 +0100

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	exit(EXIT_SUCCESS);	
+}
